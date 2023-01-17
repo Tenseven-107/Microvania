f01a08b0d64bdb9686a5bf1793021d07b83c6a1b
Tenseven-107 <78176691+Tenseven-107@users.noreply.github.com>
2023-01-17 13:58:00 +0100

    Some wip on the generator

--- Scripts/Util/Loot_generator.gd
+++ Scripts/Util/Loot_generator.gd
@@ -3,7 +3,15 @@ class_name Loot_generator
 
 
 export (int) var max_number: int = 5
-export (Array, PackedScene) var items: Array
+
+
+var items: Dictionary = {
+	#"name": {object, chance}
+	0: {"object": null, "chance": 5},
+	1: {"object": preload("res://Prefabs/Items/Misc/HP_pickup.tscn"), "chance": 5},
+	2: {"object": preload("res://Prefabs/Items/Misc/Big_HP_pickup.tscn"), "chance": 2}
+}
+
 
 var item_container = null
 
@@ -11,36 +19,43 @@ var item_container = null
 func _ready():
 	randomize()
 
-	items.resize(max_number)
-
 	GlobalSignals.connect("spawn_item", self, "spawn")
 
 
 
 # Spawn item
 func spawn(pos):
-	var picked_number = pick()
-
-	if picked_number:
-		var inst = items[picked_number].instance()
-		inst.global_position = pos
-
-		item_container.call_deferred("add_child", inst)
+	print(pick())
 
 
 
 #  Pick item
 func pick():
-	if items.size() > 0:
-		var number: int = int(rand_range(0, max_number))
-		var picked_item
+	var total_chance: int = 0
+	var done: bool = false
+	var all_items = items.values()
+	var size = items.size()
 
-		items.shuffle()
-		if items[number] != null:
-			picked_item = number
-		return picked_item
+	for item in size:
+		var item_data = all_items[item]
+		var chance = item_data.get("chance", null)
+		total_chance += chance
 
+		if item == (size - 1):
+			done = true
 
+	if done:
+		var random_number: int = int(rand_range(0, total_chance))
+
+		for item in size:
+			var gotten_item = all_items[item].get("object", null)
+			var item_chance = all_items[item].get("chance", null)
+
+			if total_chance and item_chance > random_number:
+				return gotten_item
+
+			else:
+				return null
 
 
 
